fe7dfca7d58ba5d9cd9125d5028565759868a524
jellyfishgh <1129088994@qq.com>
2016-04-20 00:17:01 +0800

    add promise demos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var router = require('koa-router')();
+var fs = require('fs');
 
-router.get('/', function *(next) {
-  yield this.render('index', {
-    title: 'Hello World Koa!'
-  });
-});
+router.get('/', generator);
+router.get('index', generator);
+
+function* generator(next) {
+    var jsonStr = fs.readFileSync('../public/bookmarks.json', "utf-8");
+    console.log(jsonStr);
+    var marks = JSON.parse(jsonStr);
+    console.log(marks);
+    yield this.render('index', {
+        title: 'Hello World Koa!'
+        // marks: marks
+    });
+}
 
 module.exports = router;
